9b8323353bf3abfd6c61c00bf71685975bf54db9
luan <luan@ridi.com>
2018-08-31 09:59:33 +0900

    Create constant classes

---
+++ src/Ridibooks/Store/Library/AccountCommandApiClient/Constant/JobPriorityConstant.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Ridibooks\Store\Library\AccountCommandApiClient\Constant;
+
+class JobPriorityConstant
+{
+    // 범위: 0 ~ 255
+    public const LOW = 255;
+    public const DEFAULT = 128;
+    public const HIGH = 64;
+}
