39225ab5ff6cfb2ca497e9e52dfc3a01e3190060
luan <luan@ridi.com>
2018-09-03 15:47:23 +0900

    Fix phpdocs on deprecated Book class

--- src/Ridibooks/Store/Library/AccountCommandApiClient/Book.php
+++ src/Ridibooks/Store/Library/AccountCommandApiClient/Book.php
@@ -6,7 +6,7 @@ namespace Ridibooks\Store\Library\AccountCommandApiClient;
 use Ridibooks\Store\Library\AccountCommandApiClient\Model\LibraryItemFull;
 
 /**
- * @deprecated Use \Ridibooks\Store\Library\AccountCommandApiClient\LibraryItemFull
+ * @deprecated Use \Ridibooks\Store\Library\AccountCommandApiClient\Model\LibraryItemFull
  */
 class Book extends LibraryItemFull
 {
