f0d7cc49f15b5b7ccae8fe8efd758d027a730cd0
luan <luan@ridi.com>
2018-09-03 16:22:29 +0900

    Rename constants in JobPriorityConstant

--- src/Ridibooks/Store/Library/AccountCommandApiClient/Constant/JobPriorityConstant.php
+++ src/Ridibooks/Store/Library/AccountCommandApiClient/Constant/JobPriorityConstant.php
@@ -3,10 +3,23 @@ declare(strict_types=1);
 
 namespace Ridibooks\Store\Library\AccountCommandApiClient\Constant;
 
+/*
+ * 범위: 0 ~ 255
+ * 사용처가 있는 값만 정의한다.
+ *
+ * 0            - HIGHEST
+ * 1 ~ 127      - HIGHxx
+ * 128          - DEFAULT
+ * 129 ~ 254    - LOWxx
+ * 255          - LOWEST
+ */
 class JobPriorityConstant
 {
-    // 범위: 0 ~ 255
-    public const LOW = 255;
+    public const LOWEST = 255;
+
+    public const LOW192 = 192;
+
     public const DEFAULT = 128;
-    public const HIGH = 64;
+
+    public const HIGH64 = 64;
 }
